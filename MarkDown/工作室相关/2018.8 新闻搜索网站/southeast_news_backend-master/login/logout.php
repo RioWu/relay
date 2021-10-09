3d494b7f632dc8c20456db0542df483dc1a3e24a
RioChen <283489710@qq.com>
2021-10-10 01:45:47 +0800

    delete something which isn't in project

--- MarkDown/工作室相关/2018.8 新闻搜索网站/southeast_news_backend-master/login/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_destroy();
-$_SESSION = array();
-$logout = array('success' => 1, 'errorcode' => '200', 'errormessage' => '');
-echo $logout;
-?>
--- MarkDown/常用文档/github api token.md
+++
@@ -1,1 +0,0 @@
- github api token   `03e2ea795d403f118c74c97f22b4790717db166e` 
